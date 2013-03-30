fafd0b4cbab406b899621d73b6de0b1caf9f2a20
David Warring <david.warring@gmail.com>
2013-03-31 07:25:44 +1100

    adding css1 todo list/stubs

--- lib/CSS/Vocabulary/CSS1.pm
+++ lib/CSS/Vocabulary/CSS1.pm
@@ -2,31 +2,101 @@ use v6;
 
 grammar CSS::Vocabulary::CSS1 {
 
-    rule decl:sym<background-attachment> {:i (background\-attachment) ':' [
-                                               [ scroll | fixed ] & <ident>
-                                               | <inherit> || <bad_args> ]}
+    # Fonts
+    # - font-family: [[<family-name> | <generic-family>],]* [<family-name> | <generic-family>]
+    # - font-style: normal | italic | oblique
+    rule decl:sym<font\-style> {:i (font\-style) ':' [
+                                     [ normal |bold | oblique ] & <ident>
+                                     | <inherit> || <bad_args> ] }
 
+    # - font-variant: normal | small-caps
+    # - font-weight: normal | bold | bolder | lighter | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900
+    # - font-size: <absolute-size> | <relative-size> | <length> | <percentage>
+    # - font: [ <font-style> || <font-variant> || <font-weight> ]? <font-size> [ / <line-height> ]? <font-family>
+    # - color: <color>
+    # Backgrounds
+    # - background-color: <color> | transparent
     rule decl:sym<background-color> {:i (background\-color) ':' [
                                           <color>
                                           | [ fixed & <ident> ]
                                           | <inherit> || <bad_args> ]}
 
+    # - background-image: <url> | none
     rule decl:sym<background-image> {:i (background\-image) ':' [
                                           <url>
                                           | [ fixed & <ident> ]
                                           | <inherit> || <bad_args> ]}
 
+    # - background-repeat: repeat | repeat-x | repeat-y | no-repeat
+    rule decl:sym<background-repeat> {:i (background\-repeat) ':' [
+                                          [ repeat[\-[x|y]]? | no\-repeat ] & <ident>
+                                          | <inherit> || <bad_args> ]}
+
+
+    # - background-attachment: scroll | fixed
+    rule decl:sym<background-attachment> {:i (background\-attachment) ':' [
+                                               [ scroll | fixed ] & <ident>
+                                               | <inherit> || <bad_args> ]}
+
+    # - background-position: [<percentage> | <length>]{1,2} | [top | center | bottom] || [left | center | right]
     rule decl:sym<background-position> {:i (background\-position) ':' [
                                           [ <percentage> | <length> ]**1..2 | [ top | center | bottom ] & <ident>
                                           [[ left | center | right ] & <ident> ]?
                                           | <inherit> || <bad_args> ]}
 
-    rule decl:sym<background-repeat> {:i (background\-repeat) ':' [
-                                          [ repeat[\-[x|y]]? | no\-repeat ] & <ident>
-                                          | <inherit> || <bad_args> ]}
-
-    rule decl:sym<font\-style> {:i (font\-style) ':' [
-                                     [ normal |bold | oblique ] & <ident>
-                                     | <inherit> || <bad_args> ] }
+    # - background: <background-color> || <background-image> || <background-repeat> || <background-attachment> || <background-position>
+    # Text
+    # - word-spacing: normal | <length>
+    # - letter-spacing: normal | <length>
+    # - text-decoration: none | [ underline || overline || line-through || blink ]
+    # - vertical-align: baseline | sub | super | top | text-top | middle | bottom | text-bottom | <percentage>
+    # - text-transform: capitalize | uppercase | lowercase | none
+    # - text-align: left | right | center | justify
+    # - text-indent: <length> | <percentage>
+    # - line-height: normal | <number> | <length> | <percentage>
+    # - margin-top: <length> | <percentage> | auto
+    # - margin-right: <length> | <percentage> | auto
+    # - margin-bottom: <length> | <percentage> | auto
+    # - margin-left: <length> | <percentage> | auto
+    # - margin: [ <length> | <percentage> | auto ]{1,4}
+    # - padding-top: <length> | <percentage>
+    # - padding-right: <length> | <percentage>
+    # - padding-bottom: <length> | <percentage>
+    # - padding-left: <length> | <percentage>
+    # - padding: [ <length> | <percentage> ]{1,4}
+    # - border-top-width: thin | medium | thick | <length>
+    # - border-right-width: thin | medium | thick | <length>
+    # - border-bottom-width: thin | medium | thick | <length>
+    # - border-left-width: thin | medium | thick | <length>
+    # - border-width: [thin | medium | thick | <length>]{1,4}
+    # - border-color: <color>{1,4}
+    # - border-style: none | dotted | dashed | solid | double | groove | ridge | inset | outset
+    # - border-top: <border-top-width> || <border-style> || <color>
+    # - border-right: <border-right-width> || <border-style> || <color>
+    # - border-bottom: <border-bottom-width> || <border-style> || <color>
+    # - border-left: <border-left-width> || <border-style> || <color>
+    # - border: <border-width> || <border-style> || <color>
+    # Positioning etc
+    # - width: <length> | <percentage> | auto
+    # - height: <length> | <percentage> | auto
+    # - float: left | right | none
+    # - clear: none | left | right | both
+    # - display: block | inline | list-item | none
+    # - white-space: normal | pre | nowrap
+    # - list-style-type: disc | circle | square | decimal | lower-roman | upper-roman | lower-alpha | upper-alpha | none
+    # - list-style-image: <url> | none
+    # - list-style-position: inside | outside
+    # - list-style: <keyword> || <position> || <url>
+    # - position: absolute | relative | static
+    # - left: <length> | <percentage> | auto
+    # - top: <length> | <percentage> | auto
+    # - clip: <shape> | auto
+    # - overflow: none | clip | scroll
+    # - z-index: auto | <integer>
+    # - visibility: inherit | visible | hidden
+    # - page-break-before: auto | allways | left | right
+    # - page-break-after: auto | allways | left | right
+    # - size: <length>{1,2} | auto | portrait | landscape
+    # - marks: crop || cross | none
 
 }
--- lib/CSS/Vocabulary/CSS1/Actions.pm
+++ lib/CSS/Vocabulary/CSS1/Actions.pm
@@ -5,9 +5,17 @@ use CSS::Vocabulary::Actions;
 class CSS::Vocabulary::CSS1::Actions
     is CSS::Vocabulary::Actions {
 
-    method decl:sym<background-attachment>($/) {
-        $._make_decl($/, 'scroll | fixed | inherit');
-    };
+    method decl:sym<font-family>($/) { warn "font-family - tba" }
+
+    method decl:sym<font-style>($/) {
+        $._make_decl($/, '');
+    }
+
+    method decl:sym<font-variant>($/) { warn "font-variant - tba" }
+    method decl:sym<font-weight>($/) { warn "font-weight - tba" }
+    method decl:sym<font-size>($/) { warn "font-size - tba" }
+    method decl:sym<font>($/) { warn "font - tba" }
+    method decl:sym<color>($/) { warn "color - tba" }
 
     method decl:sym<background-color>($/) {
         $._make_decl($/, '<color> | transparent | inherit')
@@ -21,13 +29,66 @@ class CSS::Vocabulary::CSS1::Actions
         $._make_decl($/, 'repeat | repeat-x | repeat-y | no-repeat')
     };
 
+    method decl:sym<background-attachment>($/) {
+        $._make_decl($/, 'scroll | fixed | inherit');
+    };
+
     method decl:sym<background-position>($/) {
         $._make_decl($/, '[<percentage> | <length>]{1,2} | [top | center | bottom] || [left | center | right]')
     };
 
-    method decl:sym<font-style>($/) {
-        $._make_decl($/, '');
-    }
+    method decl:sym<background>($/) { warn "background - tba" }
+    method decl:sym<word-spacing>($/) { warn "word-spacing - tba" }
+    method decl:sym<letter-spacing>($/) { warn "letter-spacing - tba" }
+    method decl:sym<text-decoration>($/) { warn "text-decoration - tba" }
+    method decl:sym<vertical-align>($/) { warn "vertical-align - tba" }
+    method decl:sym<text-transform>($/) { warn "text-transform - tba" }
+    method decl:sym<text-align>($/) { warn "text-align - tba" }
+    method decl:sym<text-indent>($/) { warn "text-indent - tba" }
+    method decl:sym<line-height>($/) { warn "line-height - tba" }
+    method decl:sym<margin-top>($/) { warn "margin-top - tba" }
+    method decl:sym<margin-right>($/) { warn "margin-right - tba" }
+    method decl:sym<margin-bottom>($/) { warn "margin-bottom - tba" }
+    method decl:sym<margin-left>($/) { warn "margin-left - tba" }
+    method decl:sym<margin>($/) { warn "margin - tba" }
+    method decl:sym<padding-top>($/) { warn "padding-top - tba" }
+    method decl:sym<padding-right>($/) { warn "padding-right - tba" }
+    method decl:sym<padding-bottom>($/) { warn "padding-bottom - tba" }
+    method decl:sym<padding-left>($/) { warn "padding-left - tba" }
+    method decl:sym<padding>($/) { warn "padding - tba" }
+    method decl:sym<border-top-width>($/) { warn "border-top-width - tba" }
+    method decl:sym<border-right-width>($/) { warn "border-right-width - tba" }
+    method decl:sym<border-bottom-width>($/) { warn "border-bottom-width - tba" }
+    method decl:sym<border-left-width>($/) { warn "border-left-width - tba" }
+    method decl:sym<border-width>($/) { warn "border-width - tba" }
+    method decl:sym<border-color>($/) { warn "border-color - tba" }
+    method decl:sym<border-style>($/) { warn "border-style - tba" }
+    method decl:sym<border-top>($/) { warn "border-top - tba" }
+    method decl:sym<border-right>($/) { warn "border-right - tba" }
+    method decl:sym<border-bottom>($/) { warn "border-bottom - tba" }
+    method decl:sym<border-left>($/) { warn "border-left - tba" }
+    method decl:sym<border>($/) { warn "border - tba" }
+    method decl:sym<width>($/) {  warn "width - tba" }
+    method decl:sym<height>($/) {  warn "height - tba" }
+    method decl:sym<float>($/) { warn "float - tba" }
+    method decl:sym<clear>($/) { warn "clear - tba" }
+    method decl:sym<display>($/) { warn "display - tba" }
+    method decl:sym<white-space>($/) { warn "white-space - tba" }
+    method decl:sym<list-style-type>($/) { warn "list-style-type - tba" }
+    method decl:sym<list-style-image>($/) { warn "list-style-image - tba" }
+    method decl:sym<list-style-position>($/) { warn "list-style-position - tba" }
+    method decl:sym<list-style>($/) { warn "list-style - tba" }
+    method decl:sym<position>($/) { warn "position - tba" }
+    method decl:sym<left>($/) { warn "left - tba" }
+    method decl:sym<top>($/) { warn "top - tba" }
+    method decl:sym<clip>($/) { warn "clip - tba" }
+    method decl:sym<overflow>($/) { warn "overflow - tba" }
+    method decl:sym<z-index>($/) { warn "z-index - tba" }
+    method decl:sym<visibility>($/) { warn "visibility - tba" }
+    method decl:sym<page-break-before>($/) { warn "page-break-before - tba" }
+    method decl:sym<page-break-after>($/) { warn "page-break-after - tba" }
+    method decl:sym<size>($/) { warn "size - tba" }
+    method decl:sym<marks>($/) { warn "marks - tba" }
 
 
 }
