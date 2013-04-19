dfb852d1ad752d5f6e811200e4f01ad6a63f3aed
David Warring <david.warring@gmail.com>
2013-04-19 15:47:48 +1000

    consolidated extension classes

--- lib/CSS/Language/CSS3/Actions.pm
+++
@@ -1,8 +0,0 @@
-use v6;
-
-use CSS::Extensions::CSS3::Colors;
-use CSS::Language::CSS21::Actions;
-
-class CSS::Language::CSS3::Actions
-    is CSS::Extensions::CSS3::Colors::Actions
-    is CSS::Language::CSS21::Actions { };
