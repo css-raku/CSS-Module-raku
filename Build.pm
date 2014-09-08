f700699a2b71967a067672c2d8f4af7c5670942c
David Warring david.warring@gmail.com <david.warring@gmail.com>
2014-09-07 22:05:29 -0700

    Converted Build.pm to Panda::Builder. Removed generated Spec dirs from source

--- Build.pm
+++ Build.pm
@@ -1,43 +1,58 @@
 use v6;
 
 use CSS::Specification::Build;
+use Panda::Builder;
+use Panda::Common;
 
-multi MAIN( Bool :$interfaces=True, Bool :$grammars=True, Bool :$actions=True ) {
+class Build is Panda::Builder {
 
-    our &build = &CSS::Specification::Build::generate;
+    method build($where, Bool :$interfaces=True, Bool :$grammars=True, Bool :$actions=True ) {
+        say "Building: {$where}/";
 
-    for ('etc/css1-properties.txt' => <CSS1>,
-         'etc/css21-properties.txt' => <CSS21>,
-         'etc/css3x-font-properties.txt' => <CSS3 Fonts>,
-         'etc/css3x-font-@fontface-properties.txt' => <CSS3 Fonts AtFontFace>,
-         'etc/css3x-paged-media.txt' => <CSS3 PagedMedia>,
-        ) {
-        my ($input-spec, $class-isa) = .kv;
+        indir $where, {
+            for ('etc/css1-properties.txt' => <CSS1>,
+                 'etc/css21-properties.txt' => <CSS21>,
+                 'etc/css3x-font-properties.txt' => <CSS3 Fonts>,
+                 'etc/css3x-font-@fontface-properties.txt' => <CSS3 Fonts AtFontFace>,
+                 'etc/css3x-paged-media.txt' => <CSS3 PagedMedia>,
+                ) {
+                my ($input-spec, $class-isa) = .kv;
 
-        my @productions;
+                my @productions;
 
-        @productions.push: 'interface'    => 'Interface'
-            if $interfaces;
+                @productions.push: 'interface'    => 'Interface'
+                    if $interfaces;
 
-        @productions.push: 'actions'   => 'Actions'
-            if $actions;
+                @productions.push: 'actions'   => 'Actions'
+                    if $actions;
 
-        @productions.push: 'grammar' => 'Grammar'
-            if $grammars;
+                @productions.push: 'grammar' => 'Grammar'
+                    if $grammars;
 
-        for @productions {
-            my ($type, $subclass) = .kv;
-            my $name = (<CSS Module>, @$class-isa, <Spec>,  $subclass).join('::');
-            my $class-path = (<lib CSS Module>, @$class-isa, <Spec>, $subclass).join('/') ~ '.pm';
-            my %opts = proforma  =>  $class-isa !=== <CSS1>
-                if $type eq 'grammar';
+                for @productions {
+                    my ($type, $subclass) = .kv;
+                    my $name = (<CSS Module>, @$class-isa, <Spec>,  $subclass).join('::');
 
-            note "building $input-spec => $type: $name";
-            temp $*IN  = open $input-spec, :r;
-            temp $*OUT = open $class-path, :w;
+                    my $class-dir = (<lib CSS Module>, @$class-isa, <Spec>).join('/');
+                    mkdir $class-dir;
 
-            build( $type, $name, |%opts );
+                    my $class-path = $class-dir ~ '/' ~ $subclass ~ '.pm';
+
+                    my %opts = proforma  =>  $class-isa !=== <CSS1>
+                        if $type eq 'grammar';
+
+                    say "Building $input-spec => $type: $name";
+                    temp $*IN  = open $input-spec, :r;
+                    temp $*OUT = open $class-path, :w;
+
+                    CSS::Specification::Build::generate( $type, $name, |%opts );
+                }
+            }
         }
     }
 }
-     
+
+# this Build.pm can also be run standalone    
+sub MAIN(Str $working-directory = '.', Bool :$interfaces=True, Bool :$grammars=True, Bool :$actions=True ) {
+    Build.new.build($working-directory, :$interfaces, :$grammars, :$actions);
+}
