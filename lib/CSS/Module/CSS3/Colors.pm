7597447c02c16e1c2320b8e0f83f8fdd0846ffde
David Warring david.warring@gmail.com <david.warring@gmail.com>
2014-10-14 12:17:32 -0700

    make use of <usage> token

--- lib/CSS/Module/CSS3/Colors.pm
+++ lib/CSS/Module/CSS3/Colors.pm
@@ -50,28 +50,31 @@ grammar CSS::Module::CSS3::Colors:ver<20110607.000>
     rule color-alpha{<number>}
 
     # <rgb> and <hex> are defined in CSS core grammar
+    #| usage: rgba(c,c,c,a) where c is 0..255 or 0%-100% and a is 0-1 or 0%-100%
     rule color:sym<rgba> {:i'rgba('
                               [ <r=.color-range> ','
                                 <g=.color-range> ','
                                 <b=.color-range> ','
-                                <a=.color-alpha> || <any-args> ]
+                                <a=.color-alpha> || :my $*USAGE; <usage(&?ROUTINE.WHY)> ]
                    ')'
     }
 
     rule percentage-range {<percentage>}
 
+    #| usage: hsl(h,s,l) where h is 0..360  and s,l are 0-1 or 0%-100%
     rule color:sym<hsl> {:i'hsl('
                              [ <h=.color-angle> ','
                                <s=.percentage-range> ','
-                               <l=.percentage-range> || <any-args> ]
+                               <l=.percentage-range> || :my $*USAGE; <usage(&?ROUTINE.WHY)> ]
                     ')'
     }
 
+    #| usage: hsla(h,s,l,a) where h is 0..360  and s,l,a are 0-1 or 0%-100%
     rule color:sym<hsla> {:i'hsla('
                               [ <h=.color-angle> ','
                                 <s=.percentage-range> ','
                                 <l=.percentage-range> ','
-                                <a=.color-alpha> || <any-args> ]
+                                <a=.color-alpha> || :my $*USAGE; <usage(&?ROUTINE.WHY)> ]
                    ')'
     }
 
@@ -249,24 +252,18 @@ class CSS::Module::CSS3::Colors::Actions
         make $.token($percentage, :units('%'), :type('percentage'))
     }
 
-    #| usage: rgba(c,c,c,a) where c is 0..255 or 0%-100% and a is 0-1 or 0%-100%
     method color:sym<rgba>($/) {
-        return $.warning(&?ROUTINE.WHY)
-            if $<any-args>;
+        return $.warning( $<usage>.ast ) if $<usage>;
         make $.token($.node($/), :type<color>, :units<rgba>);
     }
 
-    #| usage: hsl(h,s,l) where h is 0..360  and s,l are 0-1 or 0%-100%
     method color:sym<hsl>($/)  {
-        return $.warning(&?ROUTINE.WHY)
-            if $<any-args>;
+        return $.warning( $<usage>.ast ) if $<usage>;
         make $.token($.node($/), :type<color>, :units<hsl>);
     }
 
-    #| usage: hsla(h,s,l,a) where h is 0..360  and s,l,a are 0-1 or 0%-100%
     method color:sym<hsla>($/) {
-        return $.warning(&?ROUTINE.WHY)
-            if $<any-args>;
+        return $.warning( $<usage>.ast ) if $<usage>;
         make $.token($.node($/), :type<color>, :units<hsla>);
     }
 
