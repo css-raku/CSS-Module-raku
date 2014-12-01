25f4bbd18de273ac16d6873403237a5ad9b67a3d
David Warring david.warring@gmail.com <david.warring@gmail.com>
2014-12-01 14:27:13 -0800

    move color charts to CSS::Grammar (for CSS::Writer)

--- lib/CSS/Module/_Base/Actions.pm
+++ lib/CSS/Module/_Base/Actions.pm
@@ -8,6 +8,8 @@ class CSS::Module::_Base::Actions
     is CSS::Specification::_Base::Actions
     is CSS::Grammar::Actions {
 
+    use CSS::Grammar::AST;
+
     # ---- CSS::Grammar overrides ---- #
 
     method any-function($/)             {
@@ -54,25 +56,7 @@ class CSS::Module::_Base::Actions
         make $.token(0, :type(CSSValue::AngleComponent))
     }
 
-    has Hash $.colors = {
-            black   => [   0,   0,   0 ],
-            silver  => [ 192, 192, 192 ],
-            gray    => [ 128, 128, 128 ],
-            white   => [ 255, 255, 255 ],
-            maroon  => [ 128,   0,   0 ],
-            red     => [ 255,   0,   0 ],
-            orange  => [ 255, 165,   0 ],
-            purple  => [ 128,   0, 128 ],
-            fuchsia => [ 255,   0, 255 ],
-            green   => [   0, 128,   0 ],
-            lime    => [   0, 255,   0 ],
-            olive   => [ 128, 128,   0 ],
-            yellow  => [ 255, 255,   0 ],
-            navy    => [   0,   0, 128 ],
-            blue    => [   0,   0, 255 ],
-            teal    => [   0, 128, 128 ],
-            aqua    => [   0, 255, 255 ],
-            };
+    has Hash $.colors = %CSS::Grammar::AST::CSS21-Colors;
 
    method color:sym<named>($/) {
         my $color-name = $<keyw>.ast;
